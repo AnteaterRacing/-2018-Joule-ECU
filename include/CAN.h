7b105ae2204c0a65d4a70b54da35fae5f43c2e64
XavierDedenbach <35046513+XavierDedenbach@users.noreply.github.com>
2018-02-20 13:35:12 -0800

    probs commiting

--- include/CAN.h
+++ include/CAN.h
@@ -9,25 +9,23 @@
 #ifndef CAN_H_
 #define CAN_H_
 
-
-//defining the ECU number to change CAN parameters when initializing.
-#define Front_ECU_CAN_Init 1
-#define Rear_ECU_CAN_Init 0
-
 //defining Message ID for front to rear ECU message transmit
 //size: 6 bytes: Accelerator, BrakeAngle, SteeringAngle, TVEnable, StartButton, FrontFault
 #define FrontToRearDataMessageID 10
 #define FrontToRearDataMessageSize 6
+#define FrontToRearDataMessageIDRef 1
 
 //defining message ID for telemetry signals
 //size: 8 bytes: Wheelspeed L, Wheelspeed R, Tire temps L1,L2,L3,R1,R2,R3
 #define FrontToRearTelemetryMessageID 20
 #define FrontToRearTelemetryMessageSize 8
+#define FrontToRearTelemetryMessageIDRef 2
 
 //defining Message ID for rear to front ECU message transmit
 //size: 7 bytes: IMDFault,BMSFault,BSPDFault, Speedometer, TractionLED, TV_LED, MotorTempLED
 #define RearToFrontDataMessageID 11
 #define RearToFrontDataMessageSize 7
+#define RearToFrontDataMessageIDRef 3
 
 
 //TODO @Ken Tran: add message ID definitions for BMS
@@ -58,9 +56,9 @@
 #define MotorTempLED	7
 
 
-void CAN_Init(uint8_t ecu); //front or rear ecu init
-void CAN_Transmit(uint16_t messageID, uint8_t* message); 	//transmits CAN message with specified messageID
-void CAN_Receive(uint16_t messageID, uint8_t* message);		//receives CAN message with specified messageID
+void CAN_Init(void); //front or rear ecu init
+void CAN_TransmitData(uint16_t, uint8_t*); 	//transmits CAN message with specified messageID
+void CAN_ReceiveData(uint16_t, uint8_t*);		//receives CAN message with specified messageID
 void delay(void);
 
 #endif /* CAN_H_ */
